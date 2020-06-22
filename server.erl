68663aadfac65bc3a0336d8f801e682f714b64e6
Juan Cruz De La Torre <delatorrejuanchi@gmail.com>
2020-06-22 18:25:45 -0300

    observe game, play game, new game, make gamelist local, remove deadlocks

--- server.erl
+++ server.erl
@@ -1,9 +1,24 @@
 -module(server).
--export([start/0, pstat/0, pbalance/1, dispatcher/1, psocket/1, pcomando/4, userlist/1, gamelist/1]).
 -define(PSTAT_INTERVAL, 10000).
 -define(DEFAULT_PORT, 8000).
 
+% -export([start/0, pstat/0, pbalance/1, dispatcher/1, psocket/1, pcomando/4, userlist/1, gamelist/1]).
+-compile(export_all).
+
 format(String, Data) -> lists:flatten(io_lib:format(String, Data)).
+
+globalize(Ids) -> [format("~s|~s", [Id, node()]) || Id <- Ids].
+
+parse_game_id(GameId) ->
+    case string:lexemes(GameId, "|") of
+        [Id, Node] ->
+            NodeAtom = list_to_atom(Node),
+            case lists:member(NodeAtom, [node() | nodes()]) of
+                true -> {Id, NodeAtom};
+                false -> invalid_game_id
+            end;
+        _ -> invalid_game_id
+    end.
 
 spawn_services(ListenSocket) ->
     {_, Port} = inet:port(ListenSocket),
@@ -12,7 +27,7 @@ spawn_services(ListenSocket) ->
     spawn(?MODULE, pstat, []),
     register(pbalance, spawn(?MODULE, pbalance, [maps:new()])),
     register(userlist, spawn(?MODULE, userlist, [maps:new()])),
-    register(gamelist, spawn(?MODULE, gamelist, [maps:new()])),
+    register(gamelist, spawn(?MODULE, gamelist, [maps:new(), 1])),
     ok.
 
 start() ->
@@ -30,8 +45,7 @@ start() ->
     end,
     ok.
 
-% TODO: cambiar estadistica
-load() -> length(erlang:ports()).
+load() -> erlang:statistics(run_queue).
 
 pstat() ->
     [{pbalance, Node} ! {update_node_loads, node(), load()} || Node <- [node() | nodes()]],
@@ -40,9 +54,9 @@ pstat() ->
 
 pbalance(NodeLoads) ->
     receive
-        {find_best_node, PSocketId} ->
+        {get_best_node, PSocketId} ->
             {BestNode, _} = lists:nth(1, lists:keysort(2, maps:to_list(NodeLoads))),
-            PSocketId ! BestNode,
+            PSocketId ! {best_node, BestNode},
             pbalance(NodeLoads);
         {update_node_loads, Node, Load} -> pbalance(maps:put(Node, Load, NodeLoads))
     end,
@@ -53,27 +67,69 @@ userlist(Users) ->
     receive
         {put_user, Socket, Username, Caller} ->
             case lists:member(Username, maps:values(Users)) of
-                true -> Caller ! error, userlist(Users);
+                true -> Caller ! username_taken, userlist(Users);
                 false -> Caller ! ok, userlist(maps:put(Socket, Username, Users))
             end;
-        {remove_user, Socket} -> userlist(maps:remove(Socket, Users));
-        {get_users, Caller} -> Caller ! Users, userlist(Users)
+        {remove_user, Socket} -> userlist(maps:remove(Socket, Users))
     end,
     ok.
 
-gamelist(Games) ->
+gamelist(Games, Id) ->
     io:format(">> Games: ~p.~n", [Games]),
     receive
-        {add_game, GameId, GameProcessId} -> gamelist(maps:put(GameId, GameProcessId, Games));
-        {create_game, _} ->
-            GameId = game_id, % TODO: generar bien
-            GameProcessId = game_process_id,
-            % GameProcessId = spawn(?MODULE, game, [[Player1, null], 1, []]),
-            [{gamelist, Node} ! {add_game, GameId, GameProcessId} || Node <- nodes()],
-            gamelist(maps:put(GameId, GameProcessId, Games));
-        {get_games, Caller} -> Caller ! maps:keys(Games), gamelist(Games)
+        {create_game, Socket} ->
+            GameId = integer_to_list(Id),
+            GameProcessId = spawn(?MODULE, game, [[], [Socket], 1, []]),
+            gamelist(maps:put(GameId, GameProcessId, Games), Id + 1);
+        {get_local_games, Caller} ->
+            Caller ! globalize(maps:keys(Games)),
+            gamelist(Games, Id);
+        {accept_game, Socket, GameId, Caller} ->
+            case maps:get(GameId, Games, invalid_game_id) of
+                invalid_game_id -> Caller ! invalid_game_id;
+                GameProcessId ->
+                    GameProcessId ! {acc, Socket, self()},
+                    receive
+                        ok -> Caller ! ok;
+                        game_full -> Caller ! game_full
+                    end
+            end,
+            gamelist(Games, Id);
+        {observe_game, Socket, GameId, Caller} ->
+            case maps:get(GameId, Games, invalid_game_id) of
+                invalid_game_id -> Caller ! invalid_game_id;
+                GameProcessId -> GameProcessId ! {obs, Socket}, Caller ! ok
+            end,
+            gamelist(Games, Id);
+        {player_move, Socket, GameId, Move, Caller} ->
+            case maps:get(GameId, Games, invalid_game_id) of
+                invalid_game_id -> Caller ! invalid_game_id;
+                GameProcessId ->
+                    GameProcessId ! {pla, Socket, Move, self()},
+                    receive
+                        ok -> Caller ! ok;
+                        invalid_move -> Caller ! invalid_move
+                    end
+            end,
+            gamelist(Games, Id)
     end,
     ok.
+
+get_games(Node) ->
+    {gamelist, Node} ! {get_local_games, self()},
+    receive Games -> Games end.
+
+game(Board, Players, Turn, Observers) ->
+    receive
+        {acc, Player2, Caller} ->
+            case Players of
+                [Player1] -> Caller ! ok, game(Board, [Player1, Player2], Turn, Observers);
+                [_, _] -> Caller ! game_full, game(Board, Players, Turn, Observers)
+            end;
+        {obs, Observer} -> game(Board, Players, Turn, [Observer | Observers]);
+        {pla, _, _, Caller} -> Caller ! ok, game(Board, Players, Turn, Observers)
+    end.
+
 
 dispatcher(ListenSocket) ->
     case gen_tcp:accept(ListenSocket) of
@@ -83,23 +139,25 @@ dispatcher(ListenSocket) ->
             dispatcher(ListenSocket);
         {error, Reason} ->
             io:format(">> Error: ~p.~n", [Reason])
-            % cerrar el resto de las cosas?
+            % TODO: cerrar el resto de las cosas?
     end,
     ok.
 
 psocket(Socket) ->
     case gen_tcp:recv(Socket, 0) of
         {ok, Data} ->
-            pbalance ! {find_best_node, self()},
+            pbalance ! {get_best_node, self()},
             receive
-                BestNode ->
+                {best_node, BestNode} ->
+                    io:format(user, ">> Ejecutando pcomando de ~p en ~p.~n", [Socket, BestNode]),
                     spawn(BestNode, ?MODULE, pcomando, [Data, self(), Socket, node()]),
                     receive
-                        {con, _} -> % {con, Username}
+                        {con, Username} ->
+                            io:format(">> Se ha asociado el nombre ~p a ~p.~n", [Username, Socket]),
                             gen_tcp:send(Socket, "OK"),
                             psocket(Socket);
                         bye ->
-                            io:format(">> Se ha desconectado el cliente ~p.~n", [Socket]),
+                            io:format(">> Se ha desconectado ~p.~n", [Socket]),
                             gen_tcp:send(Socket, "OK"),
                             gen_tcp:close(Socket);
                         {lsg, CMDID, Games} ->
@@ -110,27 +168,24 @@ psocket(Socket) ->
                             end,
                             psocket(Socket);
                         {new, CMDID} ->
+                            io:format(">> Se ha creado un nuevo juego para ~p.~n", [Socket]),
                             gen_tcp:send(Socket, format("OK ~s", [CMDID])),
                             psocket(Socket);
-                        % {acc, CMDID, JuegoId} ->
-                        %     io:format(">> TODO"),
-                        %     gen_tcp:send("OK " ++ CMDID)
-                        %     psocket(Socket, Username);
-                        % {pla, CMDID, JuegoId, Jugada} ->
-                        %     io:format(">> TODO"),
-                        %     gen_tcp:send("OK " ++ CMDID)
-                        %     psocket(Socket, Username);
-                        % {obs, CMDID, JuegoId} ->
-                        %     io:format(">> TODO"),
-                        %     gen_tcp:send("OK " ++ CMDID)
-                        %     psocket(Socket, Username);
-                        % {lea, CMDID, JuegoId} ->
-                        %     io:format(">> TODO"),
-                        %     gen_tcp:send("OK " ++ CMDID)
-                        %     psocket(Socket, Username);
-                        error ->
-                            % abstraer correctamente errores. cada comando erroneo deberia recibir correctamente su mensaje ERROR op1 op2...
-                            io:format(">> Error: no se pudo procesar el comando.~n"),
+                        {acc, CMDID, GameId} ->
+                            io:format(">> Se ha aceptado a ~p en el juego ~p.~n", [Socket, GameId]),
+                            gen_tcp:send(Socket, format("OK ~s", [CMDID])),
+                            psocket(Socket);
+                        {pla, CMDID, GameId, Move} ->
+                            io:format(">> ~p realizó el movimiento ~p en el juego ~p.~n", [Socket, Move, GameId]),
+                            gen_tcp:send(Socket, format("OK ~s", [CMDID])),
+                            psocket(Socket);
+                        {obs, CMDID, GameId} ->
+                            io:format(">> ~p está observando el juego ~p.~n", [Socket, GameId]),
+                            gen_tcp:send(Socket, format("OK ~s", [CMDID])),
+                            psocket(Socket);
+                        {error, Reason} ->
+                            gen_tcp:send(Socket, format("ERROR ~s", [Reason])),
+                            io:format(">> Error: no se pudo procesar el comando.~n"), % TODO: cambiar esto
                             psocket(Socket);
                         _ -> io:format(">> NOT IMPLEMENTED~n"), psocket(Socket)
                     end
@@ -143,35 +198,59 @@ psocket(Socket) ->
     ok.
 
 pcomando(Data, PSocketId, Socket, CallerNode) ->
-    % io:format(user, ">> Ejecutando pcomando de ~p para ~p.~n", [ Caller]),
+    io:format(user, ">> Ejecutando pcomando de ~p para ~p.~n", [Socket, CallerNode]),
     Lexemes = string:lexemes(string:trim(Data), " "),
     case Lexemes of
         ["CON", Username] ->
             {userlist, CallerNode} ! {put_user, Socket, Username, self()},
             receive
                 ok -> PSocketId ! {con, Username};
-                error -> PSocketId ! {error}
+                username_taken -> PSocketId ! {error, format("~s username_taken", [Username])}
             end;
         ["BYE"] ->
-            % dessubscribir de eventos
+            % TODO: dessubscribir de eventos
             {userlist, CallerNode} ! {remove_user, Socket},
             PSocketId ! bye;
         [CMD, CMDID | Args] ->
             case {CMD, Args} of
-                {"LSG", []} -> gamelist ! {get_games, self()}, receive Games -> PSocketId ! {lsg, CMDID, Games} end;
+                {"LSG", []} ->
+                    Games = [get_games(Node) || Node <- [node() | nodes()]],
+                    PSocketId ! {lsg, CMDID, lists:concat(Games)};
                 {"NEW", []} -> gamelist ! {create_game, Socket}, PSocketId ! {new, CMDID};
-                {"ACC", [JuegoId]} -> PSocketId ! {acc, CMDID, JuegoId};
-                {"PLA", [JuegoId, Jugada]} -> PSocketId ! {pla, CMDID, JuegoId, Jugada};
-                {"OBS", [JuegoId]} -> PSocketId ! {obs, CMDID, JuegoId};
-                {"LEA", [JuegoId]} -> PSocketId ! {lea, CMDID, JuegoId};
-                _ -> PSocketId ! error
+                {"ACC", [GameId]} ->
+                    case parse_game_id(GameId) of
+                        invalid_game_id -> PSocketId ! {error, format("~s invalid_game_id", [CMDID])};
+                        {Id, Node} ->
+                            {gamelist, Node} ! {accept_game, Socket, Id, self()},
+                            receive
+                                ok -> PSocketId ! {acc, CMDID, GameId};
+                                game_full -> PSocketId ! {error, format("~s game_full", [CMDID])};
+                                invalid_game_id -> PSocketId ! {error, format("~s invalid_game_id", [CMDID])}
+                            end
+                    end;
+                {"PLA", [GameId, Move]} ->
+                    case parse_game_id(GameId) of
+                        invalid_game_id -> PSocketId ! {error, format("~s invalid_game_id", [CMDID])};
+                        {Id, Node} ->
+                            {gamelist, Node} ! {player_move, Socket, Id, Move, self()},
+                            receive
+                                ok -> PSocketId ! {pla, CMDID, GameId, Move};
+                                invalid_move -> PSocketId ! {error, format("~s invalid_move", [CMDID])}
+                            end
+                    end;
+                {"OBS", [GameId]} ->
+                    case parse_game_id(GameId) of
+                        invalid_game_id -> PSocketId ! {error, format("~s invalid_game_id", [CMDID])};
+                        {Id, Node} ->
+                            {gamelist, Node} ! {observe_game, Socket, Id, self()},
+                            receive
+                                ok -> PSocketId ! {obs, CMDID, GameId};
+                                invalid_game_id -> PSocketId ! {error, format("~s invalid_move", [CMDID])}
+                            end
+                    end;
+                {"LEA", [GameId]} -> PSocketId ! {lea, CMDID, GameId};
+                _ -> PSocketId ! {error, format("~s invalid_command", [CMDID])}
             end;
-        _ -> PSocketId ! error
+        _ -> PSocketId ! {error, "invalid_command"}
     end,
     ok.
-
-% game([Player1, null], Turn, Observers) ->
-%     receive
-%         {acc, Player2} -> game([Player1, Player2], Turn, Observers)
-%     end;
-% game([Players], Turn, Observers) -> ok.
